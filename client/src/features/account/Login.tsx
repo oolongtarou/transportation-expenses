b446975edb9a099b2a8bd40c5480ca912487af4f
yusuke <oolongtarou@gmail.com>
2024-08-11 02:13:04 +0900

    design: ログイン画面のトップのdiv要素をLoginコンポーネントから削除した。

--- client/src/features/account/Login.tsx
+++ client/src/features/account/Login.tsx
@@ -5,27 +5,25 @@ import '../../styles/button.css'
 
 const Login = () => {
     return (
-        <div className='container flex items-center justify-center min-h-screen'>
-            <div className='max-w-md w-full'>
-                <div className="grid w-full items-center gap-1.5 mb-5">
-                    <Label htmlFor="email" className='text-basic'>メールアドレス</Label>
-                    <Input type="email" id="email" placeholder="example@example.com" />
-                </div>
-
-                <div className="grid w-full items-center gap-1.5 mb-5">
-                    <Label htmlFor="password">パスワード</Label>
-                    <Input type="password" id="password" placeholder="password" />
-                </div>
-                <div className='flex justify-center mb-5'>
-                    <a href='#' className='text-pale-blue text-sm'>パスワードを忘れた場合</a>
-                </div>
-                <Button className='btn btn-primary w-full mb-10'>ログイン</Button>
-                <div className='flex justify-center mb-1'>
-                    <p className='text-pale-blue text-sm'>アカウントを持っていない場合</p>
-                </div>
-                <Button className='btn btn-light w-full'>アカウントを作成する</Button>
+        <>
+            <div className="grid w-full items-center gap-1.5 mb-5">
+                <Label htmlFor="email" className='text-basic'>メールアドレス</Label>
+                <Input type="email" id="email" placeholder="example@example.com" />
             </div>
-        </div>
+
+            <div className="grid w-full items-center gap-1.5 mb-5">
+                <Label htmlFor="password">パスワード</Label>
+                <Input type="password" id="password" placeholder="password" />
+            </div>
+            <div className='flex justify-center mb-5'>
+                <a href='#' className='text-pale-blue text-sm'>パスワードを忘れた場合</a>
+            </div>
+            <Button className='btn btn-primary w-full mb-10'>ログイン</Button>
+            <div className='flex justify-center mb-1'>
+                <p className='text-pale-blue text-sm'>アカウントを持っていない場合</p>
+            </div>
+            <Button className='btn btn-light w-full'>アカウントを作成する</Button>
+        </>
     )
 }
 
