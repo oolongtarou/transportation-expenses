f8fb24bf536dc761844a30f0df3b135d260001cb
yusuke <oolongtarou@gmail.com>
2024-08-11 02:32:23 +0900

    update: グローバスで使用するcssをsrc/styles/global配下に移動した。

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './reset.css'
+import './styles/global/index.css'
+import './styles/global/reset.css'
+import './styles/global/button.css'
+
+import PasswordReset from './features/account/PasswordReset'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <div className='container flex items-center justify-center min-h-screen'>
+      <div className='max-w-md w-full'>
+        <PasswordReset />
+      </div>
+    </div>
   </StrictMode>,
 )
