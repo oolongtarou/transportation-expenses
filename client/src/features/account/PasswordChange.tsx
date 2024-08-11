0deb66910973999316416b4755d1a10fd706fa1a
yusuke <oolongtarou@gmail.com>
2024-08-11 23:41:12 +0900

    design: パスワード変更画面のデザインを作成した。

---
+++ client/src/features/account/PasswordChange.tsx
@@ -0,0 +1,35 @@
+import { Label } from "@/components/ui/label"
+import PasswordRulesDescription from "./PasswordRulesDescription"
+import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/button"
+
+const PasswordChange = () => {
+    return (
+        <div className="container max-w-md">
+            <h2 className="font-bold text-2xl text-center mb-5">パスワードを変更する</h2>
+            <PasswordRulesDescription />
+
+            <div className="grid w-full items-center gap-1.5 mb-7">
+                <Label htmlFor="password">現在のパスワード</Label>
+                <Input type="password" id="password" placeholder="password" className="mt-2" />
+            </div>
+
+            <div className="grid w-full items-center gap-1.5 mb-7">
+                <Label htmlFor="password">新しいパスワード</Label>
+                <Input type="password" id="password" placeholder="password" className="mt-2" />
+            </div>
+
+            <div className="grid w-full items-center gap-1.5 mb-7">
+                <Label htmlFor="password">新しいパスワード(確認用)</Label>
+                <Input type="password" id="password" placeholder="password" className="mt-2" />
+            </div>
+
+            <div className="flex justify-evenly">
+                <Button className="btn btn-light" >キャンセル</Button>
+                <Button className="btn btn-primary">パスワードを変更する</Button>
+            </div>
+        </div>
+    )
+}
+
+export default PasswordChange
