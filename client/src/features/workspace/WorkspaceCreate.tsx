dee8afe0431fea5f6fcbfd4c4e33d5e1709f8ad3
yusuke <oolongtarou@gmail.com>
2024-08-11 23:54:53 +0900

    design: ワークスペース作成画面のデザインを作成した。

---
+++ client/src/features/workspace/WorkspaceCreate.tsx
@@ -0,0 +1,34 @@
+import { Badge } from "@/components/ui/badge"
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import { Textarea } from "@/components/ui/textarea"
+
+
+const WorkspaceCreate = () => {
+    return (
+        <div className="container max-w-md">
+            <h2 className="font-bold text-2xl text-center mb-7">ワークスペースを作成する</h2>
+
+            <div>
+                <div>
+                    <Label htmlFor="workspaceName">ワークスペース名</Label>
+                    <Badge variant="destructive" className="ml-1">必須</Badge>
+                </div>
+                <Input type="text" id="workspaceName" className="mt-2 mb-7" />
+            </div>
+
+            <div>
+                <Label htmlFor="description">説明</Label>
+                <Textarea id="description" className="mt-2 mb-7" />
+            </div>
+
+            <div className="flex justify-evenly">
+                <Button className="btn btn-light">キャンセル</Button>
+                <Button className="btn btn-primary">ワークスペースを作成する</Button>
+            </div>
+        </div>
+    )
+}
+
+export default WorkspaceCreate
