c2ec05e978684b6bb3c4f9599e90847535ffa476
yusuke <oolongtarou@gmail.com>
2024-08-15 14:21:52 +0900

    update: ローカルでの開発のためにRateLimitをコメントアウトする。

--- server/src/app.ts
+++ server/src/app.ts
@@ -46,14 +46,14 @@ const redisClient = new Redis(redisUrl, {
     password: process.env.REDIS_PASSWORD,
 });
 
-const limiter = rateLimit({
-    windowMs: 15 * 60 * 1000,
-    max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
-    standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
-    legacyHeaders: false, // Disable the `X-RateLimit-*` headers
-});
+// const limiter = rateLimit({
+//     windowMs: 15 * 60 * 1000,
+//     max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+//     standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+//     legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+// });
 
-app.use(limiter);
+// app.use(limiter);
 
 
 app.use(session({
