bab7ffba0b757b82107e009240ede31d3901d7ec
yusuke <oolongtarou@gmail.com>
2024-08-19 23:28:41 +0900

    feat: 承認ルート画面を表示するときにスケルトンローディングするようにした。

--- client/src/features/workspace/ApprovalRoute.tsx
+++ client/src/features/workspace/ApprovalRoute.tsx
@@ -1,6 +1,7 @@
 import { Button } from "@/components/ui/button"
 import { Label } from "@/components/ui/label"
 import { Select, SelectContent, SelectGroup, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select"
+import { Skeleton } from "@/components/ui/skeleton"
 import { Table, TableBody, TableCell, TableRow } from "@/components/ui/table"
 import { WorkspaceApprovers } from "@/lib/approver"
 import { useAuth } from "@/lib/auth"
@@ -12,11 +13,12 @@ import { useLocation } from "react-router-dom"
 const ApprovalRoute = () => {
     const { currentWorkspace } = useAuth();
     const location = useLocation();
-
+    const [isLoading, setLoading] = useState(true);
     const [approvalStep, setApprovalStep] = useState<number | null>(null);
     const [workspaceApprovers, setWorkspaceApprovers] = useState<WorkspaceApprovers | null>(null);
 
     useEffect(() => {
+        setLoading(true);
         axios.get<WorkspaceApprovers>(`${import.meta.env.VITE_SERVER_DOMAIN}/workspace/approvers`, { params: { workspaceId: getWorkspaceIdFrom(location.pathname) ?? 0 }, withCredentials: true })
             .then(response => {
                 setWorkspaceApprovers(response.data);
@@ -24,6 +26,8 @@ const ApprovalRoute = () => {
             })
             .catch((err: AxiosError) => {
                 console.error(err.code);
+            }).finally(() => {
+                setLoading(false);
             })
     }, [currentWorkspace, location.pathname]);
 
@@ -33,25 +37,33 @@ const ApprovalRoute = () => {
             const userNames = workspaceApprovers?.approvers
                 .filter(approver => approver.approvalStep === step)
                 .map(approver => approver.userName) || [];
-
             rows.push(
-                <TableRow key={step} className={`${step > (approvalStep ?? 0) ? 'bg-gray-200' : ''}`}>
-                    <TableCell className="w-20">
-                        <img src="/icons/person.svg" style={{ backgroundColor: '#F0F2F5', borderRadius: '0.75rem' }} />
-                    </TableCell>
-                    <TableCell className="min-w-48">
-                        <p className="font-bold text-black">{step}段階目</p>
-                        <p>{userNames.join(', ')}</p>
-                    </TableCell>
-                    <TableCell className="text-right">
-                        {step > (approvalStep ?? 0)
-                            ? <></>
-                            : userNames.length === 0
-                                ? <Button className="btn btn-action">承認者を設定する</Button>
-                                : <Button className="btn btn-light">承認者を変更する</Button>
-                        }
-                    </TableCell>
-                </TableRow>
+                isLoading
+                    ? <TableRow>
+                        <TableCell className="h-12 w-12">
+                            <Skeleton className="h-12 w-12" />
+                        </TableCell>
+                        <TableCell className="text-left">
+                            <Skeleton className="h-12 w-96" />
+                        </TableCell>
+                    </TableRow>
+                    : <TableRow key={step} className={`${step > (approvalStep ?? 0) ? 'bg-gray-200' : ''}`}>
+                        <TableCell className="w-20">
+                            <img src="/icons/person.svg" style={{ backgroundColor: '#F0F2F5', borderRadius: '0.75rem' }} />
+                        </TableCell>
+                        <TableCell className="min-w-48">
+                            <p className="font-bold text-black">{step}段階目</p>
+                            <p>{userNames.join(', ')}</p>
+                        </TableCell>
+                        <TableCell className="text-right">
+                            {step > (approvalStep ?? 0)
+                                ? <></>
+                                : userNames.length === 0
+                                    ? <Button className="btn btn-action">承認者を設定する</Button>
+                                    : <Button className="btn btn-light">承認者を変更する</Button>
+                            }
+                        </TableCell>
+                    </TableRow>
             );
         }
         return rows;
@@ -63,34 +75,39 @@ const ApprovalRoute = () => {
                 <h2 className="heading-2">承認ルート</h2>
                 <div className="max-w-52">
                     <Label>必要な承認回数</Label>
-                    {approvalStep !== null && (
-                        <Select defaultValue={approvalStep.toString()}>
-                            <SelectTrigger id="applicationStatus" className="min-w-[80px] mt-1">
-                                <SelectValue />
-                            </SelectTrigger>
-                            <SelectContent>
-                                <SelectGroup>
-                                    <SelectItem value="1">1段階</SelectItem>
-                                    <SelectItem value="2">2段階</SelectItem>
-                                    <SelectItem value="3">3段階</SelectItem>
-                                    <SelectItem value="4">4段階</SelectItem>
-                                    <SelectItem value="5">5段階</SelectItem>
-                                </SelectGroup>
-                            </SelectContent>
-                        </Select>
-                    )}
+                    {isLoading
+                        ? <Skeleton className="h-12 w-52" />
+                        : approvalStep !== null && (
+                            <Select defaultValue={approvalStep.toString()}>
+                                <SelectTrigger id="applicationStatus" className="min-w-[80px] mt-1">
+                                    <SelectValue />
+                                </SelectTrigger>
+                                <SelectContent>
+                                    <SelectGroup>
+                                        <SelectItem value="1">1段階</SelectItem>
+                                        <SelectItem value="2">2段階</SelectItem>
+                                        <SelectItem value="3">3段階</SelectItem>
+                                        <SelectItem value="4">4段階</SelectItem>
+                                        <SelectItem value="5">5段階</SelectItem>
+                                    </SelectGroup>
+                                </SelectContent>
+                            </Select>
+                        )}
                 </div>
             </header>
             <main>
                 <h3 className="font-bold text-lg mb-2">承認者</h3>
-                <Table className="bg-white">
+                <Table className="bg-white mb-5">
                     <TableBody>
                         {renderRows()}
                     </TableBody>
                 </Table>
-                <div className="mt-3 flex justify-end mr-4">
-                    <Button className="btn btn-primary">設定を保存する</Button>
-                </div>
+                {isLoading
+                    ? <></>
+                    : <div className="flex justify-end mr-4 mb-5">
+                        <Button className="btn btn-primary">設定を保存する</Button>
+                    </div>
+                }
             </main>
         </div >
     )
