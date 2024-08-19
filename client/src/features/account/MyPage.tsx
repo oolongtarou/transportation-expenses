2ab6d853fef597eb8021818cc309101cebc6430c
yusuke <oolongtarou@gmail.com>
2024-08-19 23:11:19 +0900

    feat: マイページを表示するときにスケルトンローディングするようにした。

--- client/src/features/account/MyPage.tsx
+++ client/src/features/account/MyPage.tsx
@@ -1,8 +1,10 @@
 import { Button } from "@/components/ui/button"
+import { Skeleton } from "@/components/ui/skeleton";
 import { Table, TableBody, TableCell, TableRow } from "@/components/ui/table"
 import { getLabelByAuthorityId } from "@/lib/auth";
 import { User } from "@/lib/user";
 import { getWorkspaceIdFrom } from "@/lib/user-workspace";
+import { waitFor } from "@/lib/utils";
 import axios, { AxiosError } from "axios";
 import { useEffect, useState } from "react";
 import { Link, useLocation } from "react-router-dom";
@@ -11,34 +13,59 @@ import { useNavigate } from "react-router-dom";
 const MyPage = () => {
     const navigate = useNavigate();
     const location = useLocation();
+    const [isLoading, setLoading] = useState(true);
     const [user, setUser] = useState<User | null>(null);
 
     useEffect(() => {
-        axios.get(`${import.meta.env.VITE_SERVER_DOMAIN}/auth/status`, { withCredentials: true })
-            .then((response) => {
-                if (response.data.userId) {
-                    setUser(response.data);
-                } else {
-                    navigate('/');
-                }
-            })
-            .catch((err: AxiosError) => {
-                console.error(`サーバーエラーが発生しました：${err.code}`);
-                navigate("/");
-            });
+        setLoading(true);
+        waitFor(3).then(() => {
+            axios.get(`${import.meta.env.VITE_SERVER_DOMAIN}/auth/status`, { withCredentials: true })
+                .then((response) => {
+                    if (response.data.userId) {
+                        setUser(response.data);
+                    } else {
+                        navigate('/');
+                    }
+                })
+                .catch((err: AxiosError) => {
+                    console.error(`サーバーエラーが発生しました：${err.code}`);
+                    navigate("/");
+                }).
+                finally(() => {
+                    setLoading(false);
+                })
+        })
+
     }, [navigate]);
 
     return (
         <div className="container max-w-3xl">
             <h2 className="heading-2">マイページ</h2>
             <div className="flex mb-5">
-                <img src="/icons/default_user_icon.svg" className="w-32 h-32 btn-img btn-link" />
+                {isLoading
+                    ? <Skeleton className="h-32 w-32" />
+                    : <img src="/icons/default_user_icon.svg" className="w-32 h-32 btn-img btn-link" />
+                }
+
                 <div className="ml-3 pr-5 max-w-2xl">
                     <div className="flex flex-row items-center">
-                        <h3 className="font-bold text-xl mr-5">{user?.userName}</h3>
-                        <img src="/icons/edit.svg" className="btn-img btn-link" />
+                        <h3 className="font-bold text-xl mr-5">
+                            {isLoading
+                                ? <Skeleton className="h-12 w-32" />
+                                : user?.userName
+                            }
+                        </h3>
+                        {isLoading
+                            ? <></>
+                            : <img src="/icons/edit.svg" className="btn-img btn-link" />
+                        }
                     </div>
-                    <p className="text-pale-blue">{user?.mailAddress}</p>
+                    <p className="text-pale-blue">
+                        {isLoading
+                            ? <Skeleton className="h-8 w-52 mt-3" />
+                            : user?.mailAddress
+                        }
+                    </p>
                 </div>
             </div>
 
@@ -46,29 +73,42 @@ const MyPage = () => {
                 <h3 className="font-bold text-2xl mb-2 ml-4">ワークスペース</h3>
                 <Table className="bg-white mb-5">
                     <TableBody>
-                        {!user
-                            ? <></>
-                            : user.workspaces.map(workspace => (
-                                <TableRow key={workspace.workspaceId} className="flex justify-between items-center">
+                        {isLoading
+                            ? <>
+                                <TableRow>
                                     <TableCell className="flex flex-row gap-3">
-                                        <img src="/icons/default_workspace_icon.svg" className="btn-img btn-light" style={{ cursor: 'auto' }} />
-                                        <div>
-                                            <p className="text-black font-bold">{workspace.workspaceName}</p>
-                                            <p>{user.authorities
-                                                .filter(authority => authority.workspaceId === workspace.workspaceId)
-                                                .map(authority => getLabelByAuthorityId(authority.authorityId)).join(', ')}</p>
-                                        </div>
+                                        <Skeleton className="h-12 w-12" />
+                                        <Skeleton className="h-12 w-96" />
                                     </TableCell>
                                 </TableRow>
-                            ))}
+                            </>
+                            : !user
+                                ? <></>
+                                : user.workspaces.map(workspace => (
+                                    <TableRow key={workspace.workspaceId} className="flex justify-between items-center">
+                                        <TableCell className="flex flex-row gap-3">
+                                            <img src="/icons/default_workspace_icon.svg" className="btn-img btn-light" style={{ cursor: 'auto' }} />
+                                            <div>
+                                                <p className="text-black font-bold">{workspace.workspaceName}</p>
+                                                <p>{user.authorities
+                                                    .filter(authority => authority.workspaceId === workspace.workspaceId)
+                                                    .map(authority => getLabelByAuthorityId(authority.authorityId)).join(', ')}
+                                                </p>
+                                            </div>
+                                        </TableCell>
+                                    </TableRow>
+                                ))}
 
                     </TableBody>
                 </Table>
             </div>
-            <div className="flex justify-end gap-5 mb-5">
-                <Link to={`/w/${getWorkspaceIdFrom(location.pathname)}/account/password/change`} className="btn btn-light">パスワードを変更する</Link>
-                <Button className="btn btn-danger">アカウントを削除する</Button>
-            </div>
+            {isLoading
+                ? <></>
+                : <div className="flex justify-end gap-5 mb-5">
+                    <Link to={`/w/${getWorkspaceIdFrom(location.pathname)}/account/password/change`} className="btn btn-light">パスワードを変更する</Link>
+                    <Button className="btn btn-danger">アカウントを削除する</Button>
+                </div>
+            }
         </div >
     )
 }
