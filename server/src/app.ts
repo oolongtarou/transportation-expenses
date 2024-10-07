48c28cb16d073dd15ee47c3da50755d4fd25f1ea
oolongtarou <71446730+oolongtarou@users.noreply.github.com>
2024-10-07 15:33:10 +0900

    Update app.ts

--- server/src/app.ts
+++ server/src/app.ts
@@ -66,8 +66,8 @@ app.use(session({
     cookie: {
         httpOnly: true,
         // secure: process.env.NODE_ENV === 'production',
-        secure: false,
-        sameSite: 'lax',
+        secure: true,
+        sameSite: 'none',
         maxAge: 1000 * 60 * 60 * 24,
     },
     store: new RedisStore({
