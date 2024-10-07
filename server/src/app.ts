a0a5fe82c36dbeab1139a317773f71cbbf75c9c9
oolongtarou <71446730+oolongtarou@users.noreply.github.com>
2024-10-07 15:25:24 +0900

    Update app.ts

--- server/src/app.ts
+++ server/src/app.ts
@@ -67,7 +67,7 @@ app.use(session({
         httpOnly: true,
         // secure: process.env.NODE_ENV === 'production',
         secure: false,
-        sameSite: 'none',
+        sameSite: 'lax',
         maxAge: 1000 * 60 * 60 * 24,
     },
     store: new RedisStore({
