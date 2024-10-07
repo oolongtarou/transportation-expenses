3c060215d3abb5ef730bec8a0883c8875d9c4f5b
oolongtarou <71446730+oolongtarou@users.noreply.github.com>
2024-10-07 15:10:15 +0900

    Update app.ts

--- server/src/app.ts
+++ server/src/app.ts
@@ -50,20 +50,21 @@ const redisClient = new Redis(redisUrl, {
 
 const limiter = rateLimit({
     windowMs: 15 * 60 * 1000,
-    max: 1000, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+    max: 10000, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
     standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
     legacyHeaders: false, // Disable the `X-RateLimit-*` headers
 });
 
 app.use(limiter);
 
-app.set('trust proxy', true);
+// app.set('trust proxy', 3);
 
 app.use(session({
     secret: process.env.SESSION_SECRET as string,
     resave: false,
     saveUninitialized: false,
     cookie: {
+        proxy: true,
         httpOnly: true,
         secure: process.env.NODE_ENV === 'production',
         sameSite: 'none',
