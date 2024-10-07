03ba48f9e4495143df2143fde658e05c924e28df
oolongtarou <71446730+oolongtarou@users.noreply.github.com>
2024-10-07 15:00:03 +0900

    Update app.ts

--- server/src/app.ts
+++ server/src/app.ts
@@ -78,8 +78,7 @@ app.use(session({
 
 // CORSの許可
 app.use((req, res, next) => {
-    // res.header('Access-Control-Allow-Origin', process.env.CLIENT_DOMAIN)
-    res.header('Access-Control-Allow-Origin', "*")
+    res.header('Access-Control-Allow-Origin', process.env.CLIENT_DOMAIN)
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     res.header('Access-Control-Allow-Credentials', 'true')
